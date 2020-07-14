d5f940a62120d8ade07dcefd3c2ff4d46e2e6a87
Gonza077 <63622293+Gonza077@users.noreply.github.com>
2020-07-14 12:56:50 -0300

    Rellenado de las clases Planes y PlanesDesktop

--- UI.desktop/Forms Entidades/Planes/Planes.Designer.cs
+++ UI.desktop/Forms Entidades/Planes/Planes.Designer.cs
@@ -28,12 +28,198 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Planes));
+            this.tcPlanes = new System.Windows.Forms.ToolStripContainer();
+            this.tsPlanes = new System.Windows.Forms.ToolStrip();
+            this.tsNuevo = new System.Windows.Forms.ToolStripButton();
+            this.tsEditar = new System.Windows.Forms.ToolStripButton();
+            this.tsEliminar = new System.Windows.Forms.ToolStripButton();
+            this.tlPlanes = new System.Windows.Forms.TableLayoutPanel();
+            this.dgvPlanes = new System.Windows.Forms.DataGridView();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ID_Especialidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tcPlanes.ContentPanel.SuspendLayout();
+            this.tcPlanes.TopToolStripPanel.SuspendLayout();
+            this.tcPlanes.SuspendLayout();
+            this.tsPlanes.SuspendLayout();
+            this.tlPlanes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPlanes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tcPlanes
+            // 
+            // 
+            // tcPlanes.ContentPanel
+            // 
+            this.tcPlanes.ContentPanel.Controls.Add(this.tlPlanes);
+            this.tcPlanes.ContentPanel.Size = new System.Drawing.Size(528, 272);
+            this.tcPlanes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tcPlanes.Location = new System.Drawing.Point(0, 0);
+            this.tcPlanes.Name = "tcPlanes";
+            this.tcPlanes.Size = new System.Drawing.Size(528, 297);
+            this.tcPlanes.TabIndex = 0;
+            this.tcPlanes.Text = "toolStripContainer1";
+            // 
+            // tcPlanes.TopToolStripPanel
+            // 
+            this.tcPlanes.TopToolStripPanel.Controls.Add(this.tsPlanes);
+            // 
+            // tsPlanes
+            // 
+            this.tsPlanes.Dock = System.Windows.Forms.DockStyle.None;
+            this.tsPlanes.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsNuevo,
+            this.tsEditar,
+            this.tsEliminar});
+            this.tsPlanes.Location = new System.Drawing.Point(3, 0);
+            this.tsPlanes.Name = "tsPlanes";
+            this.tsPlanes.Size = new System.Drawing.Size(81, 25);
+            this.tsPlanes.TabIndex = 0;
+            // 
+            // tsNuevo
+            // 
+            this.tsNuevo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsNuevo.Image = ((System.Drawing.Image)(resources.GetObject("tsNuevo.Image")));
+            this.tsNuevo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsNuevo.Name = "tsNuevo";
+            this.tsNuevo.Size = new System.Drawing.Size(23, 22);
+            this.tsNuevo.Text = "Nuevo";
+            this.tsNuevo.Click += new System.EventHandler(this.tsNuevo_Click);
+            // 
+            // tsEditar
+            // 
+            this.tsEditar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsEditar.Image = ((System.Drawing.Image)(resources.GetObject("tsEditar.Image")));
+            this.tsEditar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsEditar.Name = "tsEditar";
+            this.tsEditar.Size = new System.Drawing.Size(23, 22);
+            this.tsEditar.Text = "Editar";
+            this.tsEditar.Click += new System.EventHandler(this.tsEditar_Click);
+            // 
+            // tsEliminar
+            // 
+            this.tsEliminar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsEliminar.Image = ((System.Drawing.Image)(resources.GetObject("tsEliminar.Image")));
+            this.tsEliminar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsEliminar.Name = "tsEliminar";
+            this.tsEliminar.Size = new System.Drawing.Size(23, 22);
+            this.tsEliminar.Text = "Eliminar";
+            this.tsEliminar.Click += new System.EventHandler(this.tsEliminar_Click);
+            // 
+            // tlPlanes
+            // 
+            this.tlPlanes.ColumnCount = 2;
+            this.tlPlanes.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlPlanes.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tlPlanes.Controls.Add(this.dgvPlanes, 0, 0);
+            this.tlPlanes.Controls.Add(this.btnActualizar, 0, 1);
+            this.tlPlanes.Controls.Add(this.btnSalir, 1, 1);
+            this.tlPlanes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlPlanes.Location = new System.Drawing.Point(0, 0);
+            this.tlPlanes.Name = "tlPlanes";
+            this.tlPlanes.RowCount = 2;
+            this.tlPlanes.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlPlanes.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlPlanes.Size = new System.Drawing.Size(528, 272);
+            this.tlPlanes.TabIndex = 0;
+            // 
+            // dgvPlanes
+            // 
+            this.dgvPlanes.AllowUserToAddRows = false;
+            this.dgvPlanes.AllowUserToDeleteRows = false;
+            this.dgvPlanes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPlanes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Descripcion,
+            this.ID_Especialidad});
+            this.tlPlanes.SetColumnSpan(this.dgvPlanes, 2);
+            this.dgvPlanes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvPlanes.Location = new System.Drawing.Point(3, 3);
+            this.dgvPlanes.MultiSelect = false;
+            this.dgvPlanes.Name = "dgvPlanes";
+            this.dgvPlanes.ReadOnly = true;
+            this.dgvPlanes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPlanes.Size = new System.Drawing.Size(522, 237);
+            this.dgvPlanes.TabIndex = 0;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnActualizar.Location = new System.Drawing.Point(369, 246);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 1;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(450, 246);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "ID";
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // Descripcion
+            // 
+            this.Descripcion.DataPropertyName = "Descripcion";
+            this.Descripcion.HeaderText = "Descripcion";
+            this.Descripcion.Name = "Descripcion";
+            this.Descripcion.ReadOnly = true;
+            // 
+            // ID_Especialidad
+            // 
+            this.ID_Especialidad.DataPropertyName = "IDEspecialidad";
+            this.ID_Especialidad.HeaderText = "ID_Especialidad";
+            this.ID_Especialidad.Name = "ID_Especialidad";
+            this.ID_Especialidad.ReadOnly = true;
+            // 
+            // Planes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(528, 297);
+            this.Controls.Add(this.tcPlanes);
+            this.Name = "Planes";
+            this.Text = "Planes";
+            this.Load += new System.EventHandler(this.Planes_Load);
+            this.tcPlanes.ContentPanel.ResumeLayout(false);
+            this.tcPlanes.TopToolStripPanel.ResumeLayout(false);
+            this.tcPlanes.TopToolStripPanel.PerformLayout();
+            this.tcPlanes.ResumeLayout(false);
+            this.tcPlanes.PerformLayout();
+            this.tsPlanes.ResumeLayout(false);
+            this.tsPlanes.PerformLayout();
+            this.tlPlanes.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPlanes)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStripContainer tcPlanes;
+        private System.Windows.Forms.TableLayoutPanel tlPlanes;
+        private System.Windows.Forms.DataGridView dgvPlanes;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.ToolStrip tsPlanes;
+        private System.Windows.Forms.ToolStripButton tsNuevo;
+        private System.Windows.Forms.ToolStripButton tsEditar;
+        private System.Windows.Forms.ToolStripButton tsEliminar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Descripcion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID_Especialidad;
     }
 }
--- UI.desktop/Forms Entidades/Planes/Planes.cs
+++ UI.desktop/Forms Entidades/Planes/Planes.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Academia.Business.Entities;
+using Academia.Business.Logic;
 
 namespace Academia.UI.Desktop.Forms_Entidades.Planes
 {
@@ -15,6 +17,50 @@ namespace Academia.UI.Desktop.Forms_Entidades.Planes
         public Planes()
         {
             InitializeComponent();
+            dgvPlanes.AutoGenerateColumns = false;
+        }
+
+        public void Listar()
+        {
+            PlanLogic pl = new PlanLogic();
+            try
+            {
+                dgvPlanes.DataSource = pl.GetAll();
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+            }
+        }
+
+        private void Planes_Load(object sender, EventArgs e)
+        {
+            Listar();
+        }
+
+        private void tsNuevo_Click(object sender, EventArgs e)
+        {
+            PlanesDesktop formPlanes = new PlanesDesktop(ApplicationForm.ModoForm.Alta);
+            formPlanes.ShowDialog();
+            Listar();
+        }
+ 
+
+        private void tsEditar_Click(object sender, EventArgs e)
+        {
+            int ID = ((Plan)this.dgvPlanes.SelectedRows[0].DataBoundItem).ID;
+            PlanesDesktop formPlanes = new PlanesDesktop(ID,ApplicationForm.ModoForm.Modificacion);
+            formPlanes.ShowDialog();
+            Listar();
+        }
+
+        private void tsEliminar_Click(object sender, EventArgs e)
+        {
+            int ID = ((Plan)this.dgvPlanes.SelectedRows[0].DataBoundItem).ID;
+            PlanesDesktop formPlanes = new PlanesDesktop(ID, ApplicationForm.ModoForm.Baja);
+            formPlanes.ShowDialog();
+            Listar();
         }
     }
 }
