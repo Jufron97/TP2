1f034ca630bbe84abda087bf5a606bdfbe0c17c8
Juan Frontons <juan230897@gmail.com>
2020-07-17 15:19:25 -0300

    Forms comisiones

---
+++ UI.desktop/Forms Entidades/Comisiones/ComisionesDesktop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Academia.Business.Entities;
+using Academia.Business.Logic;
+
+namespace Academia.UI.Desktop.Forms_Entidades.Comisiones
+{
+    public partial class ComisionesDesktop : ApplicationForm
+    {
+        public ComisionesDesktop()
+        {
+            InitializeComponent();
+        }
+    }
+}
